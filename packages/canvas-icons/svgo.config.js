7080eb8d3c2bd928baf9da15641653f2c16ed8bd
Sheelah Brennan <sheelah@workday.com>
2026-07-15 11:42:41 -0600

    chore: Update svgo config

--- packages/canvas-icons/svgo.config.js
+++ packages/canvas-icons/svgo.config.js
@@ -10,9 +10,9 @@ module.exports = {
           // We don't want to minify IDs. [View Docs](https://svgo.dev/docs/plugins/cleanupIds)
           // It can create issues when multiple SVGs are on the same page.
           cleanupIds: false,
-          // We want to keep the viewbox. [View docs](https://svgo.dev/docs/plugins/removeViewBox/)
+          // We want to keep the viewbox; [View docs](https://svgo.dev/docs/plugins/removeViewBox/)
           removeViewBox: false,
-          // Keep fill/stroke on paths, not hoisted to parent <g>. [View docs](https://svgo.dev/docs/plugins/moveElemsAttrsToGroup/)
+          // Keep fill/stroke on paths, not hoisted to parent <g>; [View docs](https://svgo.dev/docs/plugins/moveElemsAttrsToGroup/)
           moveElemsAttrsToGroup: false,
         },
       },
